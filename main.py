1f97e689dca99bc6694a12d8ac2a553df8f7aa0f
axel <denysiuk.isi@gmail.com>
2020-08-18 15:52:04 -0300

    base

---
+++ main.py
@@ -0,0 +1,43 @@
+def sumar(a,b):
+    pass
+
+def restar(a,b):
+    pass
+
+def dividir(a,b):
+    pass
+
+def multiplicar(a,b):
+    pass
+
+def potencia(base, exponente):
+    pass
+
+print("Calculadora".center(30,"="))
+
+valor_a = float(input("Ingrese un número"))
+valor_b = float(input("Ingrese otro número"))
+
+print("Operaciones Disponibles")
+
+print("1- Suma.")
+print("2- Resta.")
+print("3- Divición.")
+print("4- Multiplicación.")
+print("5- Potencia.")
+
+operacion = 0
+while operacion not in (1,2,3,4,5):
+    operacion = int(input("Seleccione una operacion: "))
+
+if operacion==1:
+    pass
+elif operacion==2:
+    pass
+elif operacion==3:
+    pass
+elif operacion==4:
+    pass
+elif operacion==5:
+    pass
+
